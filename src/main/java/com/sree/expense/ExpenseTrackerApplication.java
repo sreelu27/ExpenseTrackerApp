a41efcf956dd69ed40e94e683acf3d00a4cc0f9c
sreelu27 <sreelekshmig27@gmail.com>
2024-08-19 20:36:45 +0100

    added swagger docs for API

--- src/main/java/com/sree/expense/ExpenseTrackerApplication.java
+++ src/main/java/com/sree/expense/ExpenseTrackerApplication.java
@@ -1,8 +1,33 @@
 package com.sree.expense;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Expense Tracker REST API Documentation",
+				description = "Expense Tracker REST API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Sree",
+						email = "abc@gmal.com",
+						url = "www.abc.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "www.abc.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Expense Tracker REST API Documentation for developers",
+				url = "www.abc.com"
+		)
+)
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
--- src/main/java/com/sree/expense/controller/CategoryController.java
+++ src/main/java/com/sree/expense/controller/CategoryController.java
@@ -2,6 +2,9 @@ package com.sree.expense.controller;
 
 import com.sree.expense.dto.CategoryDto;
 import com.sree.expense.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +12,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for Category",
+        description = "CRUD REST APIs for Category"
+)
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
@@ -20,28 +27,68 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @Operation(
+            summary = "Create Category REST API",
+            description = "Create Category REST API to save category into DB"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP STATUS 201 CREATED"
+    )
     @PostMapping()
     public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto){
         return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.CREATED);
 
     }
 
+    @Operation(
+            summary = "GET Category REST API",
+            description = "GET Category REST API to get category from DB"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 OK"
+    )
     @GetMapping("{id}")
     public ResponseEntity<CategoryDto> getCategoryById(@PathVariable("id") Long id){
         return new ResponseEntity<>(categoryService.getCategoryById(id), HttpStatus.OK);
 
     }
 
+    @Operation(
+            summary = "GET All Category REST API",
+            description = "GET All Category REST API to get category from DB"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 OK"
+    )
     @GetMapping()
     public ResponseEntity<List<CategoryDto>> getAllCategories(){
         return new ResponseEntity<>(categoryService.getAllCategories(),HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Update Category REST API",
+            description = "Update Category REST API to update category to DB"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 OK"
+    )
     @PutMapping("{id}")
     public ResponseEntity<CategoryDto> updateCategory(@PathVariable("id") Long id, @RequestBody CategoryDto categoryDto){
         return new ResponseEntity<>(categoryService.updateCategory(id, categoryDto),HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete Category REST API",
+            description = "Delete Category REST API to delete category from DB"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP STATUS 200 OK"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteCategory(@PathVariable("id") Long id){
         categoryService.deleteCategoryById(id);
